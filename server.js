247b0eda6ef9adfa38ebf0618cbe46201fd1de7f
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-01 19:00:22 -0500

    added connect flash require in server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require('passport');
 const session = require('express-session');
+const flash = require('connect-flash')
 const morgan = require('morgan');
 
 // Require models for syncing
