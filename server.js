222aae635286e3ec21c9d02789df0a3b55cee422
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-13 08:34:54 -0500

    swtiched to express-flash

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
-const flash = require('connect-flash');
+const flash = require('express-flash');
 const morgan = require('morgan');
+
+const sessionStore = new session.MemoryStore();
 
 // Require models for syncing
 const db = require('./models');
@@ -16,15 +18,21 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(flash());
 
 // Passport
 app.use(
-  session({ secret: 'driverLogger', resave: true, saveUninitialized: true })
+    session({ secret: 'driverLogger', store: sessionStore, resave: true, saveUninitialized: true })
 );
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  res.locals.sessionFlash = req.session.sessionFlash;
+  delete req.session.sessionFlash;
+  next();
+});
 
 // Routes
 require('./routes/html-routes.js')(app, passport);
