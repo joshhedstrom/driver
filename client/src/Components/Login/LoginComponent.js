d4136aa21336806171aef7ed96363950cb400a34
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-20 20:13:35 -0500

    scaffolded login and signup pages

---
+++ client/src/Components/Login/LoginComponent.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Tooltip from '@material-ui/core/Tooltip';
+import style from './loginComponent.css';
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    textAlign: 'center',
+    margin: 10
+  },
+  margin: {
+    margin: theme.spacing.unit
+  },
+  buttonStyle: {
+    paddingBottom: '6%'
+  }
+});
+
+function Login(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={1} justify="center">
+        <Typography variant="headline" component="h3">
+          Log In
+        </Typography>
+        <TextField
+          id="username"
+          label="Username (required)"
+          className={classes.textField}
+          margin="normal"
+          fullWidth
+          onChange={props.usernameAction}
+        />
+        <Tooltip title="Case Sensitive">
+          <TextField
+            id="password"
+            label="Password"
+            className={classes.textField}
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            fullWidth
+            onChange={props.passwordAction}
+          />
+        </Tooltip>
+
+        <Grid container spacing={12}>
+          <Grid item xs={12} sm={3} className={classes.buttonStyle}>
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              onClick={props.submitAction}
+            >
+              Submit
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Button size="small" variant="contained" href="/signup">
+              New User
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Login);
---
+++ client/src/Components/Signup/SignupComponent.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  },
+  margin: {
+    margin: theme.spacing.unit
+  }
+});
+
+function Signup(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="headline" component="h3">
+          Create Account
+        </Typography>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="firstName"
+              label="First Name (required)"
+              className={classes.textField}
+              margin="normal"
+              fullWidth
+              onChange={props.firstNameAction}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="lastName"
+              label="Last Name (required)"
+              className={classes.textField}
+              margin="normal"
+              fullWidth
+              onChange={props.lastNameAction}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="username"
+              label="Username (required)"
+              className={classes.textField}
+              margin="normal"
+              fullWidth
+              onChange={props.usernameAction}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="password"
+              label="Password"
+              className={classes.textField}
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+              fullWidth
+              onChange={props.passwordAction}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="passwordConfirmation"
+              label="Confirm Password"
+              className={classes.textField}
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+              fullWidth
+              onChange={props.passwordConfirmAction}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={props.submitAction}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+Signup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Signup);
