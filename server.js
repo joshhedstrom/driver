d6b7a180b1f1c3623330d0545d37543ef8da3390
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-23 16:28:13 -0500

    added mongoose

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const passport = require('passport');
-const session = require('express-session');
 const morgan = require('morgan');
 const db = require('./models');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/driver_db');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -17,9 +19,6 @@ require('./routes/api-routes.js')(app);
 require('./config/passport.js')(passport, db.user);
 // app.use(routes);
 
-
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on http://localhost: ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost: ${PORT}`);
 });
