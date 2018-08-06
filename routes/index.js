67612075c3e7eaf9bb599bb1e664bb20b169a35f
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-08-06 16:16:03 -0600

    added sending build file

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const auth = require('./auth');
 router.use('/api', apiRoutes);
 router.use('/auth', auth);
 
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 module.exports = router;
