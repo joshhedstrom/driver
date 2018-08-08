1a65eb512dd50196853731b1231a2df3a35481f1
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-08-07 19:16:31 -0600

    removed unused component

--- client/src/Components/Settings/SettingsComponent.js
+++ client/src/Components/Settings/SettingsComponent.js
@@ -4,16 +4,14 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 
-
 const styles = theme => ({
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-    },
+  container: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
@@ -33,36 +31,36 @@ function SettingsComponent(props) {
         <Typography variant="headline" component="h3">
           Account Settings
         </Typography>
-        <Grid container >
-            <Grid item xs={12}></Grid>
-                  <Grid item xs={12}>
-                      <FormControlLabel
-                          control={
-                              <Switch
-                                  checked={false}
-                                  onChange={props.handleChange}
-                                  value="checkedB"
-                                  color="primary"
-                              />
-                          }
-                          label="dark theme: Coming Soon!"
-                      />
-                  </Grid>
-                  <Grid item xs={12}>
-                      {/*item*/}
-                  </Grid>
-                  <Grid item xs={12}>
-                      {/*item*/}
-                  </Grid>
-                  <Grid item xs={12}>
-                      {/*change password*/}
-                  </Grid>
-                  <Grid item xs={12}>
-                      {/*delete account*/}
-                  </Grid>
-                  <Grid item xs={12}>
-                      {/*Questions or issues? Email me @ email@email.com*/}
-                  </Grid>
+        <Grid container>
+          <Grid item xs={12} />
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={false}
+                  onChange={props.handleChange}
+                  value="checkedB"
+                  color="primary"
+                />
+              }
+              label="dark theme: Coming Soon!"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            {/*item*/}
+          </Grid>
+          <Grid item xs={12}>
+            {/*item*/}
+          </Grid>
+          <Grid item xs={12}>
+            {/*change password*/}
+          </Grid>
+          <Grid item xs={12}>
+            {/*delete account*/}
+          </Grid>
+          <Grid item xs={12}>
+            {/*Questions or issues? Email me @ email@email.com*/}
+          </Grid>
         </Grid>
       </Paper>
     </div>
