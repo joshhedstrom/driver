574de65675f04a88b12260e8fc181baec11c2a85
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-08-22 12:31:56 -0600

    scaffolded file structure and delete modal

---
+++ client/src/Components/Trips/DeleteModal/DeleteModal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class DeleteModal extends React.Component {
+
+    constructor(props){
+        console.log('props::::: ', props)
+        super(props);
+        this.state = {open: true}; 
+        this.componentWillMount()
+
+    }
+    
+    
+    componentWillMount(){
+        let open = this.props.open
+        this.setState({ open: open })
+    }
+
+  render() {
+    return (
+      <div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.props.deleteClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Delete this trip?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure you want to delete this trip? This action cannot be undone!
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.deleteClose} color="primary" autoFocus>
+              cancel
+            </Button>
+            <Button onClick={this.props.deleteTrip} color="secondary">
+              delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default DeleteModal;
