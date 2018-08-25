5ea4594fd3c4e118d9a9676103df02a9dc25da91
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-08-25 11:38:12 -0600

    changed css and test files to root instead of app

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Root.css';
 import Root from './Root';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import orange from '@material-ui/core/colors/orange';
