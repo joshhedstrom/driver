afb6e54c3339ef865b4cc43286b8f4d8c8737fc7
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-08-30 17:07:58 -0600

    start date comes from database instead of local storage

--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -24,7 +24,7 @@ class Dashboard extends Component {
     wage: 0,
     defaultWage: 0,
     description: '',
-    loading: true,
+    loading: true
   };
 
   renderRedirect = () => {
@@ -51,26 +51,23 @@ class Dashboard extends Component {
           tripStarted: res.data.tripStarted,
           loading: false,
           currentTripId: res.data.currentTripId,
-          startDate: res.data.currentTripStartTime,
+          startDate: res.data.currentTripStartTime
         });
-
-        if(res.data.tripStarted){
-
-        }
       })
       .catch(err => console.log(err));
-    this.render();
   }
 
   clearState = () => {
+    let odo = this.state.endingOdometer;
     this.setState({
-      tripStarted: false,
+      lastOdometer: odo,
       endingOdometer: 0,
       miles: 0,
       hours: 0,
       tips: 0,
-      tripCompleted: false,
-      description: ''
+      description: '',
+      tripStarted: false,
+      tripCompleted: false
     });
   };
 
@@ -100,40 +97,51 @@ class Dashboard extends Component {
       tripCompleted: this.state.tripCompleted
     };
 
-    let currentTrip = localStorage.getItem('currentTrip');
+    let tripStarted = this.state.tripStarted;
     let userUrl = `/api/user/${localStorage.getItem('userId')}`;
     axios.defaults.headers.common['Authorization'] = localStorage.getItem(
       'jwtToken'
     );
 
-    if (currentTrip) {
+    //IF TRIP IS IN PROGRESS
+    if (tripStarted) {
+      //UPDATE TRIP
+     this.setState({tripCompleted: true})
       axios
-        .put(`/api/updateTrip/${currentTrip}`, formData)
+        .put(`/api/updateTrip/${this.state.currentTripId}`, formData)
         .then(res => res)
         .catch(err => console.log(err));
-      localStorage.removeItem('currentTrip');
+      //UPDATE TRIP
       axios
         .patch(userUrl, {
           tripStarted: false,
           defaultWage: this.state.wage,
-          lastOdometer: this.state.endingOdometer
+          lastOdometer: this.state.endingOdometer,
+          currentTripId: '',
+          currentTripStartTime: 0
         })
         .then(this.clearState())
         .catch(err => console.log(err));
-    } else {
+    }
+    //IF THERE IS NO TRIP IN PROGRESS
+    else {
       this.setState({ startDate: Date.now() });
+      //NEW TRIP
       axios
         .post('/api/newTrip', formData)
         .then(res => {
-          localStorage.setItem('currentTrip', res.data._id);
           this.setState({
             tripStarted: true,
-            lastOdometer: this.state.startingOdometer
+            lastOdometer: this.state.startingOdometer,
+            currentTripId: res.data._id
           });
+          //UPDATE USER
           axios
             .patch(userUrl, {
               tripStarted: true,
-              lastOdometer: this.state.startingOdometer
+              lastOdometer: this.state.startingOdometer,
+              currentTripId: res.data._id,
+              currentTripStartTime: res.data.startDate
             })
             .then(res => res)
             .catch(err => console.log(err));
@@ -154,7 +162,7 @@ class Dashboard extends Component {
               lastOdometer={this.state.lastOdometer}
               handleSubmit={this.handleSubmit}
               handleChange={this.handleChange}
-              timePassed={this.state.startDate}
+              startDate={this.state.startDate}
               defaultWage={this.state.defaultWage}
             />
           ) : (
